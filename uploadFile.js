4596e7942f9ba4594ff226b9e99f987d96830e22
vanthanhgit <thanh.le280501@stechvn.org>
2023-01-03 12:27:52 +0700

    2

--- uploadFile.js
+++ uploadFile.js
@@ -4,6 +4,8 @@ const {google} = require('googleapis');
 const fs = require('fs');
 const path = require('path');
 
+const { exec } = require("child_process");
+
 const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET;
 const REDIRECT_URI = process.env.REDIRECT_URI;
@@ -17,18 +19,31 @@ const drive = google.drive({
     version: 'v3',
     auth: oauth2Client
 })
-const pathFile = path.join(__dirname, 'gitlab.txt')
+
+exec("tar -zcf data-backup.tar.gz data-backup", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`success`);
+});
+const pathFile = path.join(__dirname, 'data-backup.tar.gz')
+const time = new Date()
 module.exports = {
     uploadFile: async () => {
         try {
             const createFile = await drive.files.create({
                 requestBody:{
                     parents:['1K5ot1d0SkjsBU5unOEQ8dSpijRWw4RvI'],
-                    name: "file-backup",
-                    mimeType: 'application/x-tar'
+                    name: `file-backup-${time.getDate()}thg${time.getMonth() + 1},${time.getFullYear()}-${time.getHours()}h${time.getMinutes()}m${time.getSeconds()}s.tar.gz`,
+                    mimeType: 'application/gzip'
                 },
                 media: {
-                    mimeType: 'application/x-tar',
+                    mimeType: 'application/gzip',
                     body: fs.createReadStream(pathFile)
                 }
             })
